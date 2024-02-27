556a0d512910162f3fd4a87711c9de25a7479dae
l1e <l1e@DESKTOP-9K58KRP>
2024-02-27 21:11:28 +0200

    Добавьте файлы проекта.

---
+++ lab5oop/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace lab5oop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSetOfActions = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSetOfActions
+            // 
+            this.btnSetOfActions.Location = new System.Drawing.Point(39, 58);
+            this.btnSetOfActions.Name = "btnSetOfActions";
+            this.btnSetOfActions.Size = new System.Drawing.Size(405, 69);
+            this.btnSetOfActions.TabIndex = 0;
+            this.btnSetOfActions.Text = "Супермегакнопка1";
+            this.btnSetOfActions.UseVisualStyleBackColor = true;
+            this.btnSetOfActions.Click += new System.EventHandler(this.btnSetOfActions_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(39, 146);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(227, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "Супермегакнопка поглинає Прозорість";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(39, 169);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(217, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "Супермегакнопка поглинає Колір тла";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(39, 192);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(222, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "Супермегакнопка поглинає Hello world";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 15);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Прозорiсть";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(176, 15);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 37);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Колiр тла";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(313, 15);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(131, 37);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Hello world";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 706);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnSetOfActions);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSetOfActions;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ lab5oop/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab5oop
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+       
+        {
+            InitializeComponent();
+            this.BackColor = Color.LightGray;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Opacity = 1.5 - Opacity;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (this.BackColor == Color.LightGray)
+            {
+                this.BackColor = Color.Red;
+            }
+            else if (this.BackColor == Color.Red)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else if (this.BackColor == Color.Yellow)
+            {
+                this.BackColor = Color.Green;
+            }
+            else if (this.BackColor == Color.Green)
+            {
+                this.BackColor = Color.LightGray;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("hello World.\nHome заблокований з відміною карантину");
+        }
+
+        private void btnSetOfActions_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Я супермегакнопка,\nі цього мене не позбавиш!");
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                btnSetOfActions.Click += button2_Click;
+            }
+            else
+            {
+                btnSetOfActions.Click -= button2_Click;
+            }
+        }
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                btnSetOfActions.Click += button3_Click;
+            }
+            else
+            {
+                btnSetOfActions.Click -= button3_Click;
+            }
+        }
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox3.Checked)
+            {
+                btnSetOfActions.Click += button4_Click;
+            }
+            else
+            {
+                btnSetOfActions.Click -= button4_Click;
+            }
+    }
+    }
+}
